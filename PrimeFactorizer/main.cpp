628d2034219e7bbca7417e68937d3766df33e25a
shibiwhy <ian.welch19@icloud.com>
2022-09-17 17:57:33 -0400

    list prime factors using vector

--- PrimeFactorizer/main.cpp
+++ PrimeFactorizer/main.cpp
@@ -1,15 +1,24 @@
 #include <iostream>
+#include <vector>
 
 
 int getInput();
-bool isPrime(int num);
-void outputMessage(int num);
+bool isPrime(int num, std::vector<int>& primeNums);
+void outputMessage(int num, std::vector<int>& primeNums);
 
 int main() {
+
+	std::vector<int> primeNums = {};
+
+
+
 	int num = int(0);
 	num = getInput();
-	if (isPrime(num)) {
-		outputMessage(num);
+	if (isPrime(num, primeNums)) {
+		outputMessage(num, primeNums);
+	}
+	else {
+		outputMessage(num, primeNums);
 	}
 }
 
@@ -23,16 +32,25 @@ int getInput() { // TODO make this sanitize input
 }
 
 
-bool isPrime(int num) {
+bool isPrime(int num, std::vector<int>& primeNums) {
+	bool result;
 	for (int i = 2; i < num; i++) { // starts at 2 because 0, 1 are not prime
 		if (num % i == 0) {
-			return false;
+			primeNums.push_back(i);
+			if (i * i == num) {
+				primeNums.push_back(i);
+			}
+			result = false;
 		}
 	}
 	return true;
 }
 
 
-void outputMessage(int num) {
-	std::cout << "is prime";
+void outputMessage(int num, std::vector<int>& primeNums) {
+	// std::cout << "is prime";
+	
+	for (int i: primeNums) {
+		std::cout << i << " has been found as a prime factor.\n";
+	}
 }
