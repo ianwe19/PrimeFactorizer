8929151ab908bcbf4fcbffea3bfd095c1bdc4b16
shibiwhy <ian.welch19@icloud.com>
2022-09-17 16:42:06 -0400

    init

--- PrimeFactorizer/main.cpp
+++ PrimeFactorizer/main.cpp
@@ -20,7 +20,12 @@ int getInput() { // TODO make this sanitize input
 
 
 bool isPrime(int num) {
-
+	for (int i = 2; i < num; i++) {
+		if (num % i == 0) {
+			return false;
+		}
+	}
+	return true;
 }
 
 
