0a0225b28ffb30e01cba20f635e1a8a1d8ddacd5
ian <ian@DESKTOP-405MMA6>
2022-09-17 16:37:33 -0400

    Add project files.

---
+++ PrimeFactorizer/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+
+int getInput();
+bool isPrime(int num);
+void outputMessage(int num);
+
+int main() {
+
+}
+
+
+int getInput() { // TODO make this sanitize input
+	int num = int(0);
+	std::cout << "Enter a number: ";
+	std::cin >> num;
+
+	return num;
+}
+
+
+bool isPrime(int num) {
+
+}
+
+
+void outputMessage(int num) {
+
+}
