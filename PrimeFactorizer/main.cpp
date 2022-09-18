3021868ae4df1c0bd13c439b49991e10dcba3e07
shibiwhy <ian.welch19@icloud.com>
2022-09-18 10:22:29 -0400

    reduce back to 2 factors

--- PrimeFactorizer/main.cpp
+++ PrimeFactorizer/main.cpp
@@ -29,37 +29,17 @@ int getInput() { // TODO make this sanitize input
 
 // builds vector of prime factors and returns whether input is prime or composite, kind of a hot mess and really slow
 bool findPrimeFactors(int num, std::vector<int>& primeNums) {
-	if (primeNums.size() == 0) { // runs if function is not being run recursively
-		for (int i = 2; i < num; i++) {
-			if (num % i == 0) { // runs if composite
-				primeNums.clear();
-				primeNums.push_back(num / i);
-				primeNums.push_back(i);
-				break;
-			}
-		}
-	}
-	else { // runs if function is being run recursively
-		for (int i = 2; i < num; i++) {
-			for (int j = 0; j < primeNums.size(); j++) {
-				if (primeNums.at(j) == num && num % i == 0) {
-					primeNums.push_back(i); // append and write factors 
-					primeNums.at(j) = num / i;
-				}
-			}
+	for (int i = 2; i < num; i++) {
+		if (num % i == 0) { // runs if composite
+			primeNums.clear();
+			primeNums.push_back(num / i);
+			primeNums.push_back(i);
+			break;
 		}
 	}
 
 	if (primeNums.size() == 0) {
 		return true; // input number is prime
-	}
-
-	for (int n : primeNums) {
-		for (int i = 2; i < n; i++) {
-			if (n % i == 0) {
-				findPrimeFactors(n, primeNums);
-			}
-		}
 	}
 
 	return false; // input number is composite
