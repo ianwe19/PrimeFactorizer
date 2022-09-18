755311870169a13b89b95bc745c975617c26fa81
shibiwhy <ian.welch19@icloud.com>
2022-09-17 20:25:27 -0400

    improve everything

--- PrimeFactorizer/main.cpp
+++ PrimeFactorizer/main.cpp
@@ -4,22 +4,25 @@
 
 int getInput();
 bool isPrime(int num, std::vector<int>& primeNums);
-void outputMessage(int num, std::vector<int>& primeNums);
+void outputMessage(int num, std::vector<int>& primeNums, bool isPrime);
+
 
 int main() {
 
 	std::vector<int> primeNums = {};
 
-
-
 	int num = int(0);
+
 	num = getInput();
-	if (isPrime(num, primeNums)) {
-		outputMessage(num, primeNums);
+
+	/*if (isPrime(num, primeNums)) {
+		std::cout << ""
 	}
 	else {
-		outputMessage(num, primeNums);
-	}
+		
+	}*/
+
+	outputMessage(num, primeNums, isPrime(num, primeNums));
 }
 
 
@@ -34,21 +37,30 @@ int getInput() { // TODO make this sanitize input
 
 bool isPrime(int num, std::vector<int>& primeNums) {
 	bool result;
-	for (int i = 2; i < num; i++) { // starts at 2 because 0, 1 are not prime
-		if (num % i == 0) {
+
+	for (int i = 2; i < num; i++) { // starts at 2 because 0 and 1 are not prime
+		if (num % i == 0) { // runs if composite
 			primeNums.push_back(i);
 			if (i * i == num) {
 				primeNums.push_back(i);
 			}
 			result = false;
 		}
+		else { // runs if prime
+			result = true;
+		}
 	}
-	return true;
+	return result;
 }
 
 
-void outputMessage(int num, std::vector<int>& primeNums) {
-	// std::cout << "is prime";
+void outputMessage(int num, std::vector<int>& primeNums, bool isPrime) {
+	if (isPrime) {
+		std::cout << num << " is a PRIME number.\n";
+	}
+	else {
+		std::cout << num << " is a COMPOSITE number.\n";
+	}
 	
 	for (int i: primeNums) {
 		std::cout << i << " has been found as a prime factor.\n";
